edc4d2781563283effa3101b5250b37106ef954e
adityakumar1120 <adityaakumar1120@gmail.com>
2025-01-19 21:51:09 +0530

    first commit

--- script.js
+++ script.js
@@ -48,7 +48,3 @@ window.addEventListener('resize', (e)=>{
     getWindowInnerWidth()
 })
 
-const receiverName = document.querySelector("#receiver");
-const senderName = document.querySelector("#sender");
-receiverName.innerText = new URLSearchParams(location.search).get("receiver");
-senderName.innerText = new URLSearchParams(location.search).get("sender") || 'Aditya kumar';
