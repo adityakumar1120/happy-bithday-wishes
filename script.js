cf0ffd86e51eb0abf9b0631d802435cd669bc93e
adityakumar1120 <adityaakumar1120@gmail.com>
2025-01-19 19:06:31 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const btn = document.querySelector('.btn')
 const flameWrapper = document.querySelector('.flame-wrapper')
+const containerElem = document.querySelector('.container')
+const popupElem = document.querySelector('.popup-cont')
+
 
 btn.addEventListener('click' , ()=>{
     flameWrapper.classList.add('hidden')
     btn.style.background = `linear-gradient(135deg, #f63b26 10%, #F5AF19 100%)`
+    setTimeout(()=>{
+        containerElem.children[0].classList.add('hidden')
+        containerElem.children[1].classList.add('hidden')
+        setTimeout(()=>{
+            containerElem.classList.add('none')
+            popupElem.classList.remove('none')
+        },100)
+    },700)
 })
